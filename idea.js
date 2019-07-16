47ed7437ff7cfc93c461203af324aae9d8609627
Edwin Del Bosque <edwindelbosquer@gmail.com>
2019-07-16 16:40:38 -0600

    Add class, constructor and methods
    Co-Authored By: Elyse Myers @ec-myers & Jeannie Evans @jmevans0211

---
+++ idea.js
@@ -0,0 +1,25 @@
+class Idea {
+	constructor(id, title, body, star, quality) {
+		this.id = id;
+		this.title = title;
+		this.body = body;
+		this.star = star || false;
+		this.quality = quality;
+	}
+
+	saveToStorage() {
+
+	}
+
+	deleteFromStorage() {
+
+	}
+
+	updateIdea() {
+
+	}
+
+	updateQuality() {
+
+	}
+}
