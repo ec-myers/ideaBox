6254416f9ea065ba4332308f36f3424647fd4e77
“Elyse <elysemyers1@gmail.com>
2019-07-18 14:09:02 -0600

    Adds skeleton for functionality to edit a card
    Co-Authored By: Jeannie Evans @jmevans0211 & Edwin del Bosque @edwindelbosque

--- main.js
+++ main.js
@@ -23,14 +23,44 @@ btnSave.addEventListener('click', addIdea);
 // btnStarred.addEventListener('click', );
 // btnSwill.addEventListener('click', );
 cardArea.addEventListener('click', deleteCard);
+cardArea.addEventListener('click', updateCard);
 inputBody.addEventListener('keyup', handleSaveBtn);
-inputTitle.addEventListener('keyup', handleSaveBtn);
+inputTitle.addEventListener('blur', handleSaveBtn);
 window.addEventListener('DOMContentLoaded', repopulateIdeasArray);
 
 // handleBottom() {
 // 	if (e.)
 // 		make if functions to target every button/image
 // }
+
+function addIdea(e) {
+	e.preventDefault();
+	var idea = new Idea(Date.now(), inputTitle.value, inputBody.value, false, 0);
+	ideasArray.push(idea);
+	idea.saveToStorage(ideasArray);
+	handleSaveBtn();
+	addCard(idea);
+	inputTitle.value = "";
+	inputBody.value = "";
+}
+
+function addCard(object) {
+	cardArea.insertAdjacentHTML('afterbegin', `<article class="idea-card" data-id="${object.id}">
+        <header>
+          <img class="img-star" src="images/star.svg" alt="white star" id="btn-star" id="star">
+          <img class="img-delete" src="images/delete.svg" alt="delete" id="btn-delete">
+        </header>
+        <div class="card-content">
+          <h3 class="card-title" contenteditable="true">${object.title}</h3>
+          <p class="card-body" contenteditable="true">${object.body}</p>
+        </div>
+        <footer>
+          <img src="images/upvote.svg" alt="upvote" id="btn-upvote">
+          <p class="quality-text">Quality: ${object.quality}</p>
+          <img src="images/downvote.svg" alt="downvote" id="btn-downvote">    
+        </footer>
+      </article>`);
+}
 
 function repopulateIdeasArray() {
   var newArray = JSON.parse(localStorage.getItem('ideasArray')).map(function(arrayProp) {
@@ -59,35 +89,25 @@ function deleteCardFromStorage(e) {
 
 // rename above function, possibly refactor
 
-function addIdea(e) {
-	e.preventDefault();
-	var idea = new Idea(Date.now(), inputTitle.value, inputBody.value, false, 0);
-	ideasArray.push(idea);
-	idea.saveToStorage(ideasArray);
-	handleSaveBtn();
-	addCard(idea);
-	inputTitle.value = "";
-	inputBody.value = "";
-}
-
 function handleSaveBtn() {
 	btnSave.disabled = !inputTitle.value || !inputBody.value;
 }
 
-function addCard(object) {
-	cardArea.insertAdjacentHTML('afterbegin', `<article class="idea-card" data-id="${object.id}">
-        <header>
-          <img class="img-star" src="images/star.svg" alt="white star" id="btn-star" id="star">
-          <img class="img-delete" src="images/delete.svg" alt="delete" id="btn-delete">
-        </header>
-        <div class="card-content">
-          <h3>${object.title}</h3>
-          <p class="card-text">${object.body}</p>
-        </div>
-        <footer>
-          <img src="images/upvote.svg" alt="upvote" id="btn-upvote">
-          <p class="quality-text">Quality: ${object.quality}</p>
-          <img src="images/downvote.svg" alt="downvote" id="btn-downvote">    
-        </footer>
-      </article>`);
+function updateCard(e) {
+  // if (e.target.className === 'card-title' || e.target.className === 'card-body') {
+  var title = e.target.closest('.card-content').querySelector('.card-title');
+  var body = e.target.closest('.card-content').querySelector('.card-body');
+  console.log(title.innerText);
+  console.log(body.innerText);
+  // document.querySelector(".card-body");
+  // console.log(updateCard());
+
 }
+  // console.log(title);
+  //return enter key saves changes
+  //assign the new fields to the property values on DOM
+  //get the objects with the changes
+  //update the array to include new changes
+  //pass the array to local storage udpateIdea() to update data model
+
+
