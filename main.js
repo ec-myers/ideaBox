f285cd2ba9099f8b8d05135923eb156a5e83879f
“Elyse <elysemyers1@gmail.com>
2019-07-21 16:23:42 -0600

    Adds functionality to return an array of objects that match the current search terms
    Co-Authored By: Jeannie Evans @jmevans0211 & Edwin del Bosque @edwindelbosque

--- main.js
+++ main.js
@@ -1,11 +1,12 @@
 var btnSave = document.querySelector('#btn-save');
-var formInputs = document.querySelector('.form-top');
+var formInputs = document.querySelector('.section-top');
 var inputBody = document.querySelector('#input-body');
 var inputTitle = document.querySelector('#input-title');
 var cardArea = document.querySelector('.section-bottom');
 var ideasArray = []
 var qualitiesArray = ["Swill", "Plausible", "Genius"];
 var btnMenu = document.querySelector('.icons-backdrop');
+var searchInput = document.querySelector('#input-search');
 
 btnSave.addEventListener('click', addIdea);
 cardArea.addEventListener('focusout', handleFocusOut);
@@ -34,6 +35,11 @@ function handleCardButtons(e) {
 function handleFormInputs(e) {
   if (e.target.id === 'input-title' || e.target.id === 'input-body') {
     handleSaveBtn(e);
+  }
+  if (e.target.id === 'input-search') {
+    console.log(e);
+    var newArray = returnSearchArray(ideasArray, searchInput.value);
+    console.log(newArray);
   }
 }
 
@@ -162,6 +168,24 @@ function handleSaveBtn() {
 	btnSave.disabled = !inputTitle.value || !inputBody.value;
 }
 
+
+function returnSearchArray(array, searchTerms) {
+  var searchResultsArray = ideasArray.filter(function(idea) {
+    return idea.title.includes(searchTerms) || idea.body.includes(searchTerms);
+  });
+  return searchResultsArray;
+}
+
+// function displaySearchResults() {
+//   if() {
+
+//   } 
+//   if (searchInput.value === '') {
+//     repopulateCards();
+//   }
+// }
+// returnSearchArray(ideasArray, searchInput)
+
 function toggleMenu(e) {
   var btnMenu = document.querySelector('.icons-backdrop');
   var classes = ["show-aside", "hide-aside"]
@@ -180,3 +204,8 @@ function toggleMenu(e) {
   animationLoop(iconTwo, '#burger-icon-2');
   animationLoop(iconThree, '#burger-icon-3');
   }
+
+//delete ALL cards (deleteCards function) and only show cards on the DOM that are in that new array (match)
+//if search bar is empty display ALL cards again repopulateCards
+
+
